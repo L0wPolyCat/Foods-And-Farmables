567982e31bc25073e653557f3b81943f4aecddc9
lowpolycat <awesomeaiden6342@outlook.com>
2023-12-09 21:14:48 -0700

    Removed random lines of unused code and fixed bugs.

--- src/main/java/net/lowpolycat/foodsandfarmables/block/OrangeBushBlock.java
+++ src/main/java/net/lowpolycat/foodsandfarmables/block/OrangeBushBlock.java
@@ -29,49 +29,49 @@ import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class OrangeBushBlock extends BushBlock {
-    public static final int MAX_AGE = 4;
-    public static final IntegerProperty AGE = BlockStateProperties.AGE_5;
-    private static final VoxelShape SAPLING_SHAPE = Block.box(3.0D, 0.0D, 3.0D, 13.0D, 8.0D, 13.0D);
-    private static final VoxelShape MID_GROWTH_SHAPE = Block.box(1.0D, 0.0D, 1.0D, 15.0D, 16.0D, 15.0D);
+            public class OrangeBushBlock extends BushBlock {
+                public static final int MAX_AGE = 4;
+                public static final IntegerProperty AGE = BlockStateProperties.AGE_4;
+                private static final VoxelShape SAPLING_SHAPE = Block.box(3.0D, 0.0D, 3.0D, 13.0D, 8.0D, 13.0D);
+                private static final VoxelShape MID_GROWTH_SHAPE = Block.box(1.0D, 0.0D, 1.0D, 15.0D, 16.0D, 15.0D);
 
-    public OrangeBushBlock(Properties pProperties) {
-        super(pProperties);
-    }
+                public OrangeBushBlock(Properties pProperties) {
+                    super(pProperties);
+                }
 
-    @Override
-    public ItemStack getCloneItemStack(BlockGetter pLevel, BlockPos pPos, BlockState pState) {
-        return new ItemStack(ModItems.ORANGE.get());
-    }
+                @Override
+                public ItemStack getCloneItemStack(BlockGetter pLevel, BlockPos pPos, BlockState pState) {
+                    return new ItemStack(ModItems.ORANGE.get());
+                }
 
-    @Override
-    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
-        if (pState.getValue(AGE) == 0) {
-            return SAPLING_SHAPE;
-        } else {
-            return pState.getValue(AGE) < 4 ? MID_GROWTH_SHAPE : super.getShape(pState, pLevel, pPos, pContext);
-        }
-    }
+                @Override
+                public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+                    if (pState.getValue(AGE) == 0) {
+                        return SAPLING_SHAPE;
+                    } else {
+                        return pState.getValue(AGE) < 4 ? MID_GROWTH_SHAPE : super.getShape(pState, pLevel, pPos, pContext);
+                    }
+                }
 
-    /**
-     * @return whether this block needs random ticking.
-     */
-    public boolean isRandomlyTicking(BlockState pState) {
-        return pState.getValue(AGE) < 4;
-    }
+                /**
+                 * @return whether this block needs random ticking.
+                 */
+                public boolean isRandomlyTicking(BlockState pState) {
+                    return pState.getValue(AGE) < 4;
+                }
 
-    /**
-     * Performs a random tick on a block.
-     */
-    @Override
-    public void randomTick(BlockState pState, ServerLevel pLevel, BlockPos pPos, RandomSource pRandom) {
-        int i = pState.getValue(AGE);
-        if (i < 4 && pLevel.getRawBrightness(pPos.above(), 0) >= 9 && net.minecraftforge.common.ForgeHooks.onCropsGrowPre(pLevel, pPos, pState, pRandom.nextInt(5) == 0)) {
-            BlockState blockstate = pState.setValue(AGE, Integer.valueOf(i + 1));
-            pLevel.setBlock(pPos, blockstate, 2);
-            pLevel.gameEvent(GameEvent.BLOCK_CHANGE, pPos, GameEvent.Context.of(blockstate));
-            net.minecraftforge.common.ForgeHooks.onCropsGrowPost(pLevel, pPos, pState);
-            System.out.println(pState.getValue(AGE));
+                /**
+                 * Performs a random tick on a block.
+                 */
+                @Override
+                public void randomTick(BlockState pState, ServerLevel pLevel, BlockPos pPos, RandomSource pRandom) {
+                    int i = pState.getValue(AGE);
+                    if (i < 4 && pLevel.getRawBrightness(pPos.above(), 0) >= 9 && net.minecraftforge.common.ForgeHooks.onCropsGrowPre(pLevel, pPos, pState, pRandom.nextInt(5) == 0)) {
+                        BlockState blockstate = pState.setValue(AGE, Integer.valueOf(i + 1));
+                        pLevel.setBlock(pPos, blockstate, 2);
+                        pLevel.gameEvent(GameEvent.BLOCK_CHANGE, pPos, GameEvent.Context.of(blockstate));
+                        net.minecraftforge.common.ForgeHooks.onCropsGrowPost(pLevel, pPos, pState);
+                        System.out.println(pState.getValue(AGE));
         }
 
     }
